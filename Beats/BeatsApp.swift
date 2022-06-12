cb610a6b95d4989485d3f3c859472cb0b71bda01
zoop1259 <kdm432191@gmail.com>
2022-06-12 23:33:12 +0900

    Initial Commit

---
+++ Beats/BeatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatsApp.swift
+//  Beats
+//
+//  Created by 강대민 on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct BeatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beats/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Beats
+//
+//  Created by 강대민 on 2022/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
